7045ca82a41b7b6267adbb7c5e09190cc10bca77
Sandra <sandra.mstowskaa@gmail.com>
2026-03-03 13:39:17 +0100

    Initial Strapi backend

---
+++ config/middlewares.ts
@@ -0,0 +1,19 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173'], 
+      headers: ['Content-Type', 'Authorization'],
+      keepHeaderOnError: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
---
+++ src/api/category-pod/controllers/category-pod.ts
@@ -0,0 +1,7 @@
+/**
+ * category-pod controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::category-pod.category-pod');
